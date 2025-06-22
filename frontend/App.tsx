c8b112b320913dc65e414d75c472fa842dc29e28
sojiro takagi <soujirou.takagi@gmail.com>
2025-06-22 17:31:43 +0900

    Native base 削除

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NativeBaseProvider } from "native-base";
 
 import HomeScreen from "./screen/HomeScreen";
 import DetailsScreen from "./screen/DetailsScreen";
@@ -11,15 +10,13 @@ const Stack = createNativeStackNavigator();
 
 function App() {
     return (
-        <NativeBaseProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="Home" component={HomeScreen} />
-                    <Stack.Screen name="Detail" component={DetailsScreen} />
-                    <Stack.Screen name="SignUp" component={SignUpScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </NativeBaseProvider>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Detail" component={DetailsScreen} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
